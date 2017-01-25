ea4aee2a861e342e08ef1c4c145774a51fb955d2
adminuser1 <adminuser1@Administrators-Mac-mini.local>
2017-01-25 18:43:51 +0400

    added configuration file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 'use strict';
 
-var gulp = require('gulp'),
-	sass = require('gulp-sass');
-	
+var gulp 		= require('gulp'),
+	sass 		= require('gulp-sass'),
+	cleanCSS 	= require('gulp-clean-css');;
+
 var paths = {
-	scss: './helppper.scss',
-	css: './helppper.css'
+	scss: 	'./helppper.scss',
+	css: 	'./helppper.css'
 };
 
 gulp.task('compileSass', function () {
 	return gulp.src(paths.scss)
 		.pipe(sass().on('error', sass.logError))
+		.pipe(cleanCSS())
 		.pipe(gulp.dest('./'));
 })
 
